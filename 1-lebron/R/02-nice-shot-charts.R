8ed0b8265b6fbd85bd3cb184ebfb0e3d71a115b4
kingmatayo <kingmatayo.thefirst@gmail.com>
2024-12-28 01:00:21 +0300

    Updated shot charts in essay

--- 1-lebron/R/02-nice-shot-charts.R
+++ 1-lebron/R/02-nice-shot-charts.R
@@ -11,20 +11,24 @@ library(htmltools)
 library(sysfonts)
 library(plotly)
 
+# Call chart creating functions
+# Source functions
+source(file="./R/00-functions.R")
+
 # Set a grey color for consistency in the visualization
 grey <- '#818990'
 
 # Use the Chivo font (you must install this locally)
 font <- sysfonts::font_add("Chivo", "./fonts/Chivo/static/Chivo-Regular.ttf")
 
-#lebron's shots
+#Import shots data
 
 all_shots <- read.csv("./data/allshots-2003-2024.csv") 
 
 lebron_shots <- all_shots |> 
   filter(namePlayer == "LeBron James")
 
-head(lebron_shots)
+
 
 # Filter for specific seasons and scale the x and y coordinates
 lebron_shots_season <- lebron_shots |>
@@ -58,7 +62,7 @@ lebron_stats <- lebron_shots_season |>
   ungroup()
 
 # Creating the visualization
-# Step 1: Data frame with player images and labels
+# Step 1: Create data frame with player images and labels
 images <- data.frame(
   nameTeam = c( "Cleveland Cavaliers", "Los Angeles Lakers"),
   date = c("October 29, 2003", "October 29, 2023"),
@@ -69,10 +73,11 @@ images <- data.frame(
   left_join(lebron_stats, by = c("nameTeam" = "name_team")) |>
   mutate(text_label = glue("<span style='font-size:14px;'>**{toupper(label)}**</span><br>
                             <span style='font-size:14px;'>**{toupper(date)}**</span><br>
-                           <span style='font-size:12.5px;color:grey40;'>{nameTeam} · {total} shot attempts · </span><br>
-                           <span style='color:#ED254E;font-size:12.5px;'>{round(x3pt_pct,0)}% were 3PT-ers</span><br>
-                           <span style='color:#ED254E;font-size:12.5px;'>{round(mid_range_pct,0)}% were Mid-Range</span><br>
-                           <span style='color:#ED254E;font-size:12.5px;'>{round(paint_pct,0)}% were in the Paint</span>")
+                           <span style='font-size:12.5px;color:grey40;'>{nameTeam}</span><br>
+                           <span style='font-size:12.5px;color:grey40;'>{total} shot attempts</span><br>
+                           <span style='color:#FF9999;font-size:12.5px;'>{round(x3pt_pct,0)}% were 3PT-ers</span><br>
+                           <span style='color:#50C878;font-size:12.5px;'>{round(mid_range_pct,0)}% were Mid-Range</span><br>
+                           <span style='color:#99CCFF;font-size:12.5px;'>{round(paint_pct,0)}% were in the Paint</span>")
   )
 
 # Step 2: Circle crop player images
@@ -80,32 +85,29 @@ images$cropped <- cropcircles::circle_crop(
   images = images$image, border_size = 1, border_colour = "whitesmoke"
 )
 
+# Create 2 separate tables for the images
+first_game_images <- images |> 
+  filter(date_game == "20031029")
+
+year_twenty_game_images <- images |> 
+  filter(date_game == "20231029")
+
 # Step 3: Creating the short chart visualization
 
-# lebron shots for the 2 key games
+# first game shot chart
+base_chart_first_game <- create_shotchart(all_shots,"LeBron James",20031029)
 
-lebron_shots_two_games <- lebron_shots_season |> 
-  filter(dateGame %in% c("20031029", "20231029"))
-
-# the shots data and image data (below) are mixed up - come back to this later to break it down and fix it 
-# Might be best to create each shot chart separately, then add the images, also separately - food for thought
-shot_chart_draft <- 
-  # draws the NBA half court
-  BasketballAnalyzeR::drawNBAcourt(
-    ggplot(data = lebron_shots_two_games), size = 0.5, col = "grey20") +
-  
-  # shot data
-  geom_point(aes(x = x, y = y, fill = court_zone), shape = 21, color = "white", size = 2.5, alpha = 0.8) + 
+draft_chart_first_game <- base_chart_first_game + 
   
   # backdrop image with fill to create border
-  geom_image(data=images, mapping=aes(x=-20, y=6, image=cropped), color="#818990", size=0.16, asp=1/1.2) +
+  geom_image(data=first_game_images, mapping=aes(x=-20, y=6, image=cropped), color="#818990", size=0.16, asp=1/1.2) +
   
   # player image
- geom_image(data = images, aes(x = -20, y = 6, image = cropped), size = 0.15, asp = 1 / 1.2) +
+  geom_image(data = first_game_images, aes(x = -20, y = 6, image = cropped), size = 0.15, asp = 1 / 1.2) +
   
   # text per player with name, team, and stats
   
-  ggtext::geom_textbox(data = images, aes(x = -15, y = -7, label = text_label), 
+  ggtext::geom_textbox(data = first_game_images, aes(x = -15, y = -7, label = text_label), 
                        fill = NA, box.size = NA) +
   
   # Create your own discrete scale
@@ -115,9 +117,42 @@ shot_chart_draft <-
   
   scale_y_continuous(expand=c(0.1,0.2)) +
   
-  # create small multiple chart for each player across 2 columns
+  # Cartesian coordinates with fixed "aspect ratio"
+  coord_equal() +
   
-  facet_wrap(~ label, ncol = 2) +
+  # Set guides for each scale
+  guides(fill = guide_legend(override.aes=list(size=5)))
+
+draft_chart_first_game
+
+# Set the output directory
+output_dir <- "./assets/"
+
+# Save the plot in PNG format
+ggsave(paste0(output_dir, "rookie_shot_chart.png"), plot = draft_chart_first_game, width = 7.5, height = 7.5)
+
+# year twenty shot chart
+base_chart_yr_twenty_game <- create_shotchart(all_shots,"LeBron James",20231029)
+
+draft_chart_yr_twenty_game <- base_chart_yr_twenty_game + 
+  
+  # backdrop image with fill to create border
+  geom_image(data=year_twenty_game_images, mapping=aes(x=-20, y=6, image=cropped), color="#818990", size=0.16, asp=1/1.2) +
+  
+  # player image
+  geom_image(data = year_twenty_game_images, aes(x = -20, y = 6, image = cropped), size = 0.15, asp = 1 / 1.2) +
+  
+  # text per player with name, team, and stats
+  
+  ggtext::geom_textbox(data = year_twenty_game_images, aes(x = -15, y = -7, label = text_label), 
+                       fill = NA, box.size = NA) +
+  
+  # Create your own discrete scale
+  scale_fill_manual(values=rev(c("#99CCFF", "#50C878", "#FF9999"))) +
+  
+  # For position scales, a vector of range expansion constants used to add some padding around the data to ensure that they are placed some distance away from the axes
+  
+  scale_y_continuous(expand=c(0.1,0.2)) +
   
   # Cartesian coordinates with fixed "aspect ratio"
   coord_equal() +
@@ -125,4 +160,8 @@ shot_chart_draft <-
   # Set guides for each scale
   guides(fill = guide_legend(override.aes=list(size=5)))
 
-shot_chart_draft
+draft_chart_yr_twenty_game
+
+# Save the plot in PNG format
+ggsave(paste0(output_dir, "veteran_shot_chart.png"), plot = draft_chart_yr_twenty_game, width = 7.5, height = 7.5)
+
