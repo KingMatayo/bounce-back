54c68cf133fb0109cc070063cf7c9de3a81fa4e4
kingmatayo <kingmatayo.thefirst@gmail.com>
2024-12-28 16:54:45 +0300

    finalized the LeBron story

--- 1-lebron/R/02-nice-shot-charts.R
+++ 1-lebron/R/02-nice-shot-charts.R
@@ -24,6 +24,7 @@ font <- sysfonts::font_add("Chivo", "./fonts/Chivo/static/Chivo-Regular.ttf")
 #Import shots data
 
 all_shots <- read.csv("./data/allshots-2003-2024.csv") 
+df_dict_nba_teams <- read.csv("./data/df_dict_nba_teams.csv")
 
 lebron_shots <- all_shots |> 
   filter(namePlayer == "LeBron James")
@@ -48,6 +49,8 @@ lebron_shots_season <- lebron_shots |>
     )
   )
 
+#Calculate the player statistics 
+
 # Group data by dateofGame and courtzone, then calculate stats
 lebron_stats <- lebron_shots_season |>
   filter(dateGame %in% c("20031029", "20231029")) |>
@@ -71,10 +74,11 @@ images <- data.frame(
             "./assets/lebron_2023.png")
 ) |>
   left_join(lebron_stats, by = c("nameTeam" = "name_team")) |>
-  mutate(text_label = glue("<span style='font-size:14px;'>**{toupper(label)}**</span><br>
+  mutate(text_label_1 = glue("<span style='font-size:14px;'>**{toupper(label)}**</span><br>
                             <span style='font-size:14px;'>**{toupper(date)}**</span><br>
-                           <span style='font-size:12.5px;color:grey40;'>{nameTeam}</span><br>
-                           <span style='font-size:12.5px;color:grey40;'>{total} shot attempts</span><br>
+                           <span style='font-size:12.5px;color:grey40;'>{nameTeam}</span><br>"),
+         
+         text_label_2 = glue("<span style='font-size:12.5px;color:grey40;'>**{total} shot attempts**</span><br>
                            <span style='color:#FF9999;font-size:12.5px;'>{round(x3pt_pct,0)}% were 3PT-ers</span><br>
                            <span style='color:#50C878;font-size:12.5px;'>{round(mid_range_pct,0)}% were Mid-Range</span><br>
                            <span style='color:#99CCFF;font-size:12.5px;'>{round(paint_pct,0)}% were in the Paint</span>")
@@ -105,9 +109,14 @@ draft_chart_first_game <- base_chart_first_game +
   # player image
   geom_image(data = first_game_images, aes(x = -20, y = 6, image = cropped), size = 0.15, asp = 1 / 1.2) +
   
-  # text per player with name, team, and stats
+  # text_label_1 with name and team
   
-  ggtext::geom_textbox(data = first_game_images, aes(x = -15, y = -7, label = text_label), 
+  ggtext::geom_textbox(data = first_game_images, aes(x = 3.8, y = 6, label = text_label_1), 
+                       fill = NA, box.size = NA) +
+  
+  # text_label_2 with stats
+  
+  ggtext::geom_textbox(data = first_game_images, aes(x = -15, y = -7, label = text_label_2), 
                        fill = NA, box.size = NA) +
   
   # Create your own discrete scale
@@ -142,9 +151,14 @@ draft_chart_yr_twenty_game <- base_chart_yr_twenty_game +
   # player image
   geom_image(data = year_twenty_game_images, aes(x = -20, y = 6, image = cropped), size = 0.15, asp = 1 / 1.2) +
   
-  # text per player with name, team, and stats
+  # text_label_1 with name and team
   
-  ggtext::geom_textbox(data = year_twenty_game_images, aes(x = -15, y = -7, label = text_label), 
+  ggtext::geom_textbox(data = year_twenty_game_images, aes(x = 3.8, y = 6, label = text_label_1), 
+                       fill = NA, box.size = NA) +
+  
+  # text_label_2 with stats
+  
+  ggtext::geom_textbox(data = year_twenty_game_images, aes(x = -15, y = -7, label = text_label_2), 
                        fill = NA, box.size = NA) +
   
   # Create your own discrete scale
@@ -165,3 +179,42 @@ draft_chart_yr_twenty_game
 # Save the plot in PNG format
 ggsave(paste0(output_dir, "veteran_shot_chart.png"), plot = draft_chart_yr_twenty_game, width = 7.5, height = 7.5)
 
+# Final Visualizations - All Styled Up
+
+# Rookie Lebron Final Viz
+
+final_chart_first_game <- draft_chart_first_game +
+  theme(legend.position = "top", # move the legend to the top
+        legend.title = element_text(face="bold", size=12), # make the title bold and increase the size
+        plot.margin = margin(t=20), #increases the Top margin of the plot by 20
+        legend.text = element_text(size=12), #inceases size of text 
+        legend.margin = margin(rep(0,4)),# Adjusting legend margin
+        legend.key = element_blank(), # Remove default legend key
+        axis.text = element_blank(), # Remove default axit text
+        legend.justification = "center", # Move legend to the center
+        panel.background = element_blank(),# Remove panel background
+        panel.grid.minor=element_blank(),# Remove panel grid minor
+        axis.title=element_blank(), # Remove axis titles
+        axis.ticks = element_blank())# Remove axis ticks
+
+# Save the plot in PNG format
+ggsave(paste0(output_dir, "final_rookie_shot_chart.png"), plot = final_chart_first_game, width = 7.5, height = 7.5)
+
+# Veteran LeBron Final Viz
+
+final_chart_yr_twenty_game <- draft_chart_yr_twenty_game +
+  theme(legend.position = "top", # move the legend to the top
+        legend.title = element_text(face="bold", size=12), # make the title bold and increase the size
+        plot.margin = margin(t=20), #increases the Top margin of the plot by 20
+        legend.text = element_text(size=12), #inceases size of text 
+        legend.margin = margin(rep(0,4)),# Adjusting legend margin
+        legend.key = element_blank(), # Remove default legend key
+        axis.text = element_blank(), # Remove default axit text
+        legend.justification = "center", # Move legend to the center
+        panel.background = element_blank(),# Remove panel background
+        panel.grid.minor=element_blank(),# Remove panel grid minor
+        axis.title=element_blank(), # Remove axis titles
+        axis.ticks = element_blank())# Remove axis ticks
+
+# Save the plot in PNG format
+ggsave(paste0(output_dir, "final_veteran_shot_chart.png"), plot = final_chart_yr_twenty_game, width = 7.5, height = 7.5)
